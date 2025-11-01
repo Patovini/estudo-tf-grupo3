da61dd626586784ed0fee3a7945aee23f0ae6ba3
VINICIUS PATO <53836370+Patovini@users.noreply.github.com>
2025-11-01 17:02:53 -0300

    fix

--- variables.tf
+++ variables.tf
@@ -1,2 +1,27 @@
 
-# teste
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+    
+  }
+}
+
+terraform {
+  backend "remote" {
+    organization = "estudo1"
+
+    workspaces {
+      name = "sessao-3-backend"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "state_test" {
+  filename = "${path.module}/state-test.txt"
+  content  = "Este arquivo está sendo gerenciado pelo Terraform."
+}
